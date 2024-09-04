195e02aa09ed3355b62e249cb124870e4c0fbb4c
YangBinLai <gastonlai8@gmail.com>
2024-09-04 21:49:02 +0800

    Add command for auto rejection

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Models\Registration;
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// to run reject all the pending classes
+Artisan::command('reject', function () {
+    $registrations = Registration::where('status', 'pending')
+        ->where('date', '<', Carbon::today())
+        ->get();
+
+    foreach ($registrations as $registration) {
+        $registration->update(['status' => 'rejected']);
+    }
+})->purpose('Reject')->daily();
+// php artisan schedule:run
